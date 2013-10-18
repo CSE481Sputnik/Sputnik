bb2fab7623becf127c38f34c4c12510aafc84952
Conor Reisman <creisman@uw.edu>
2013-10-18 13:09:17 -0700

    removed devel dir from source control

--- catkin_ws/devel/lib/python2.7/dist-packages/learning_actionlib/msg/__init__.py
+++
@@ -1,7 +0,0 @@
-from ._GripperResult import *
-from ._GripperFeedback import *
-from ._GripperActionFeedback import *
-from ._GripperActionResult import *
-from ._GripperAction import *
-from ._GripperActionGoal import *
-from ._GripperGoal import *
--- catkin_ws/devel/share/common-lisp/ros/learning_actionlib/msg/_package_GripperResult.lisp
+++
@@ -1,6 +0,0 @@
-(cl:in-package learning_actionlib-msg)
-(cl:export '(V-VAL
-          V
-          P-VAL
-          P
-))
