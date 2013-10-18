7c037b68d3fb3d08020e297d71811b341fd8997b
David Mailhot <djmailhot@gmail.com>
2013-10-18 13:09:17 -0700

    removed devel dir from source control

--- catkin_ws/devel/share/beginner_tutorials/cmake/beginner_tutorials-msg-paths.cmake
+++
@@ -1,2 +0,0 @@
-set(beginner_tutorials_MSG_INCLUDE_DIRS "/home/team7/ROS/catkin_ws/src/beginner_tutorials/msg")
-set(beginner_tutorials_MSG_DEPENDENCIES std_msgs)
--- catkin_ws/devel/share/learning_actionlib/cmake/learning_actionlib-msg-paths.cmake
+++
@@ -1,2 +0,0 @@
-set(learning_actionlib_MSG_INCLUDE_DIRS "/home/team7/ROS/catkin_ws/devel/share/learning_actionlib/msg")
-set(learning_actionlib_MSG_DEPENDENCIES actionlib_msgs;std_msgs)
