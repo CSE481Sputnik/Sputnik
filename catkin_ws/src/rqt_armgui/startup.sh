5e4cf609edb98cc2cc77e911cafdf0d6bf37553a
Team 7 <creisman@uw.edu>
2013-10-21 15:12:06 -0700

    Added startup script to simplify arm movements

---
+++ catkin_ws/src/rqt_armgui/startup.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+gnome-terminal --tab -e "bash -c 'roscore'" \
+--tab -e "bash -c 'sleep 5 && roslaunch pr2_gazebo pr2_table_object.launch'" \
+--tab -e "bash -c 'sleep 25 && roslaunch pr2_interactive_manipulation pr2_interactive_manipulation_robot.launch sim:=true'" \
+--tab -e "bash -c 'sleep 45 && roslaunch pr2_interactive_manipulation_frontend pr2_interactive_manipulation_desktop.launch'" \
+--tab -e "bash -c 'sleep 65 && rqt'"
