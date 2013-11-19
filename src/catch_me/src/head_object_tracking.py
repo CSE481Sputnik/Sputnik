28e3dde44286912b67ffc6feaa2c29fab1a30002
sputnik <team7@c1.pr2>
2013-11-18 22:23:51 -0800

    Added weighted averaging of head movement.

--- src/catch_me/src/head_object_tracking.py
+++ src/catch_me/src/head_object_tracking.py
@@ -17,16 +17,13 @@ from geometry_msgs.msg import Point
 
 class HeadObjectTracking():
 
-    # The number of previous data points to factor into this moving average
-    DEFAULT_MAX_DATA = 10
-
-    def __init__(self, max_data_points=DEFAULT_MAX_DATA):
+    def __init__(self):
         name_space = '/head_traj_controller/point_head_action'
         self.head_client = SimpleActionClient(name_space, PointHeadAction)
         self.head_client.wait_for_server()
 
-        self.point_buffer = deque(maxlen=max_data_points)
-        self.curr_tracking_point = Point(0,0,0)
+        self.curr_tracking_point = Point(1, 0, 0.5)
+        self.point_head(self.curr_tracking_point.x, self.curr_tracking_point.y, self.curr_tracking_point.z)
 
     def new_tracking_data(self, data_x, data_y, data_z):
         """
@@ -35,21 +32,14 @@ class HeadObjectTracking():
         Points the head to a point taken as a moving average over some number of
         previous tracking data points.
         """
-        # catch the last data point
-        if len(self.point_buffer) > 0:
-            last_data_point = self.point_buffer[-1]
-        else:
-            last_data_point = Point(0,0,0)
 
-        # add a new data point
-        self.point_buffer.append(Point(data_x, data_y, data_z))
-        count = len(self.point_buffer)
+        OLD_DATA_WEIGHT = .3
 
         # calculate the moving average of the x, y, z positions
         tracking_point = self.curr_tracking_point
-        avg_x = tracking_point.x - (last_data_point.x / count) + (data_x / count)
-        avg_y = tracking_point.y - (last_data_point.y / count) + (data_y / count)
-        avg_z = tracking_point.z - (last_data_point.z / count) + (data_z / count)
+        avg_x = (self.curr_tracking_point.x * OLD_DATA_WEIGHT) + (data_x * (1 - OLD_DATA_WEIGHT))
+        avg_y = (self.curr_tracking_point.y * OLD_DATA_WEIGHT) + (data_y * (1 - OLD_DATA_WEIGHT))
+        avg_z = (self.curr_tracking_point.z * OLD_DATA_WEIGHT) + (data_z * (1 - OLD_DATA_WEIGHT))
 
         # make a new averaged point to track and point the head there
         self.curr_tracking_point = Point(avg_x, avg_y, avg_z)
@@ -60,10 +50,11 @@ class HeadObjectTracking():
         Point the head to the specified point
         """
         head_goal = PointHeadGoal()
-        head_goal.target.header.frame_id = 'torso_lift_link'
+        head_goal.target.header.frame_id = '/torso_lift_link'
         head_goal.max_velocity = .3
-        head_goal.target.point = Point(x, y, z)
+        # The transform seems to aim high. Move it down a little...
+        head_goal.target.point = Point(x, y, z - .4)
         
         self.head_client.send_goal(head_goal)
-        if (self.head_client.get_state() != GoalStatus.SUCCEEDED):
-            rospy.logwarn('Head action unsuccessful.')
+#        if (self.head_client.get_state() != GoalStatus.SUCCEEDED):
+#            rospy.logwarn('Head action unsuccessful.')
