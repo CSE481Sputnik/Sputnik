d7e4a5c0597e0ba75ff64eb8226b3586cac07480
sputnik <team7@c1.pr2>
2013-11-22 15:03:50 -0800

    Fixed transforms so marker locations are stored with a static reference point. Fixed orientation in nav goals. Robot only follows one marker (id=1). Robot performs searches if the marker hasn't been seen recently. Robot moves forward if at goal and marker hasn't been seen recently (should help see around corners).

--- src/catch_me/src/destination_service.py
+++ src/catch_me/src/destination_service.py
@@ -15,18 +15,51 @@ from catch_me import srv
 
 class DestinationService:
   def __init__(self):
+    self.tf = tf.TransformListener()
     # I need a better default value...
     self.pose = None
     rospy.Subscriber('ar_pose_marker', AlvarMarkers, self.marker_cb)
     dest_serv = rospy.Service('catch_me_destination_service', srv.DestinationService, self.service_cb)
 
   def marker_cb(self, pose_markers):
-    if len(pose_markers.markers) == 0:
-      return
-
-    rospy.loginfo('AR Marker Pose updating')
-    self.pose = pose_markers.markers[0].pose
-    self.pose.header.stamp = rospy.Time.now()
+    for i in range(0, len(pose_markers.markers)):
+      marker = pose_markers.markers[i]
+      #get marker 1
+      if marker.id == 1:
+        rospy.loginfo('AR Marker Pose updating')    
+#        #get the transform from torso_life_link to map
+#        (trans, rot) = self.tf.lookupTransform('/torso_lift_link', '/map', rospy.Time(0))
+        pose = marker.pose
+        pose.header = marker.header # Marker has the valid header
+        trans_pose = self.tf.transformPose('/map', pose)
+#        orient = pose.pose.orientation
+#        euler1 = tf.transformations.euler_from_quaternion((orient.x, orient.y, orient.z, orient.w))
+#        euler2 = tf.transformations.euler_from_quaternion(rot)
+#        orient_trans = tf.transformations.quaternion_from_euler(euler1[0] - euler2[0], euler1[1] - euler2[1], euler1[2] - euler2[2])
+        
+        #make a new pose, which is the current ar markers pose mathed into a static position relative to the map
+        '''self.pose = PoseStamped()
+        self.pose.pose.position.x = pose.pose.position.x - trans[0]
+        self.pose.pose.position.y = pose.pose.position.y - trans[1]
+        self.pose.pose.position.z = pose.pose.position.z - trans[2]
+        self.pose.pose.orientation = Quaternion(*orient_trans)
+        self.pose.pose.orientation.x = -pose.pose.orientation.x * rot[0]
+        self.pose.pose.orientation.y = -pose.pose.orientation.y * rot[1]
+        self.pose.pose.orientation.z = pose.pose.orientation.z * rot[2]
+        self.pose.pose.orientation.w = pose.pose.orientation.w * rot[3]
+        self.pose.header.stamp = rospy.Time.now()
+        self.pose.header.frame_id = '/map'
+        self.pose = PoseStamped()'''
+        trans_pose.pose.orientation.x = -trans_pose.pose.orientation.x
+        trans_pose.pose.orientation.y = -trans_pose.pose.orientation.y
+#        trans_pose.pose.orientation.z = 0
+        self.pose = trans_pose
+#        common_time = self.tf.getLatestCommonTime('/ar_marker_1', '/map')
+#        self.pose.header.stamp = common_time
+#        self.pose.header.frame_id = '/ar_marker_1'
+#        self.pose.pose = pose
+#        self.pose = self.tf.transformPose('/map', self.pose)
+         
 
   def service_cb(self, dummy):
     rospy.loginfo('Returning pose ' + str(self.pose))
--- src/catch_me/src/run_catch_me.py
+++ src/catch_me/src/run_catch_me.py
@@ -12,14 +12,21 @@ from head_object_tracking import HeadObjectTracking
 from move_base_msgs.msg import MoveBaseAction, MoveBaseActionGoal, MoveBaseGoal
 from catch_me import srv
 import threading
+from catch_me.msg import *
+from actionlib_msgs.msg import GoalStatus
+import copy 
 
 class CatchMe:
-  MIN_GOAL_TIME_MS = 5000
-
   def __init__(self):
+    self.MIN_GOAL_TIME_S = 5
     self._last_move = 0
+    self._last_pose = None
 
+    self.tf = tf.TransformListener()
+
+    rospy.loginfo('Waiting for catch_me_destination_service')
     rospy.wait_for_service('catch_me_destination_service')
+    rospy.loginfo('Connected to catch_me_destination_service')
     self.destination_service = rospy.ServiceProxy('catch_me_destination_service', srv.DestinationService)
 
     rospy.loginfo('Waiting for move_base action to come up')
@@ -27,33 +34,60 @@ class CatchMe:
     self.base_client.wait_for_server()
     rospy.loginfo('Connected to move_base action server')
 
+    rospy.loginfo('Starting head-searching action')
+    self.head_s_client = actionlib.SimpleActionClient('local_search', LocalSearchAction)
+    self.head_s_client.wait_for_server()
+    rospy.loginfo('Head-search action client started')
+
     threading.Timer(0, self.move_to_marker).start()
 
   def move_to_marker(self):
     pose_stamped = self.destination_service()
-    print pose_stamped
     
-    if pose_stamped is None:
+    if pose_stamped is None or pose_stamped.pose.header.stamp.secs == 0:
       rospy.loginfo('No marker position found')
+      if self.head_s_client.get_state() != GoalStatus.ACTIVE:
+        goal = LocalSearchGoal(True)
+        self.head_s_client.send_goal(goal)
+    else:
+      pos = pose_stamped.pose.pose.position
+      orient = pose_stamped.pose.pose.orientation
+      pos_frame_id = pose_stamped.pose.header.frame_id
+      rospy.loginfo(str(pos_frame_id) + '\n' + str(pos.x) + ',' + str(pos.y) + ',' + str(pos.z))
 
-    pos = pose_stamped.pose.pose.position
-    pos_frame_id = pose_stamped.pose.header.frame_id
-    rospy.loginfo(str(pos_frame_id) + '\n' + str(pos.x) + ',' + str(pos.y) + ',' + str(pos.z))
+      if pose_stamped.pose == self._last_pose and self.base_client.get_state() == GoalStatus.SUCCEEDED:
+        rospy.loginfo('Marker position unchanged')
+        if self.head_s_client.get_state() != GoalStatus.ACTIVE:
+          goal = LocalSearchGoal(True)
+          self.head_s_client.send_goal(goal)
+      else:
+        self._last_pose = copy.deepcopy(pose_stamped.pose)
+        pose = pose_stamped.pose
+        pose.header.stamp = rospy.Time.now()
 
-    goal = MoveBaseGoal()
-    goal.target_pose.header.frame_id = "base_link"
-    goal.target_pose.header.stamp = rospy.Time.now()
-    goal.target_pose.pose.position.x = pos.x - 0.8
-    goal.target_pose.pose.position.y = pos.y
-    goal.target_pose.pose.orientation.w = 1.0
+        goal = MoveBaseGoal()
+        goal.target_pose = pose
 
-    rospy.loginfo('Sending new goal')
-    # Cancel old goal if it exists.
-    #self.base_client.cancel_all_goals()
-    self.base_client.send_goal(goal)
-    #self.base_client.wait_for_result()
-    #rospy.loginfo(str(self.base_client.get_result()))
-    threading.Timer(MIN_GOAL_TIME_MS, self.move_to_marker).start()
+        # The robot can't move in these directions. Clear them so it's not confused
+        goal.target_pose.pose.position.z = 0
+        goal.target_pose.pose.orientation.x = 0
+        goal.target_pose.pose.orientation.y = 0
+
+
+        # If the marker position is fresh, stay behind it so we don't collide.
+        # Use pose_stamped to get original time
+        if pose_stamped.pose.header.stamp.secs + 2 > rospy.Time.now().secs:
+          goal.target_pose.pose.position.x = goal.target_pose.pose.position.x - 0.8
+
+
+        rospy.loginfo('Sending new goal. x: ' + str(goal.target_pose.pose.position.x) + ', y: ' + str(goal.target_pose.pose.position.y))
+        # Cancel old goal if it exists.
+        self.base_client.cancel_all_goals()
+        self.base_client.send_goal(goal)
+        #self.base_client.wait_for_result()
+        #rospy.loginfo(str(self.base_client.get_result()))
+
+    threading.Timer(self.MIN_GOAL_TIME_S, self.move_to_marker).start()
 
 if __name__=='__main__':
     rospy.init_node('catch_me_node')
