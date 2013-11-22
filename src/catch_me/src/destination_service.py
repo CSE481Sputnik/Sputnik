ea1835d807820169027e0c3de5750284b091e4de
sputnik <team7@c1.pr2>
2013-11-22 15:10:50 -0800

    Finished clean up of destination service.

--- src/catch_me/src/destination_service.py
+++ src/catch_me/src/destination_service.py
@@ -27,32 +27,11 @@ class DestinationService:
       #get marker 1
       if marker.id == 1:
         rospy.loginfo('AR Marker Pose updating')    
-#        #get the transform from torso_life_link to map
-#        (trans, rot) = self.tf.lookupTransform('/torso_lift_link', '/map', rospy.Time(0))
         pose = marker.pose
         pose.header = marker.header # Marker has the valid header
         trans_pose = self.tf.transformPose('/map', pose)
-#        orient = pose.pose.orientation
-#        euler1 = tf.transformations.euler_from_quaternion((orient.x, orient.y, orient.z, orient.w))
-#        euler2 = tf.transformations.euler_from_quaternion(rot)
-#        orient_trans = tf.transformations.quaternion_from_euler(euler1[0] - euler2[0], euler1[1] - euler2[1], euler1[2] - euler2[2])
-        
-        #make a new pose, which is the current ar markers pose mathed into a static position relative to the map
-        '''self.pose = PoseStamped()
-        self.pose.pose.position.x = pose.pose.position.x - trans[0]
-        self.pose.pose.position.y = pose.pose.position.y - trans[1]
-        self.pose.pose.position.z = pose.pose.position.z - trans[2]
-        self.pose.pose.orientation = Quaternion(*orient_trans)
-        self.pose.pose.orientation.x = -pose.pose.orientation.x * rot[0]
-        self.pose.pose.orientation.y = -pose.pose.orientation.y * rot[1]
-        self.pose.pose.orientation.z = pose.pose.orientation.z * rot[2]
-        self.pose.pose.orientation.w = pose.pose.orientation.w * rot[3]
-        self.pose.header.stamp = rospy.Time.now()
-        self.pose.header.frame_id = '/map'
-        self.pose = PoseStamped()'''
         trans_pose.pose.orientation.x = -trans_pose.pose.orientation.x
         trans_pose.pose.orientation.y = -trans_pose.pose.orientation.y
-#        trans_pose.pose.orientation.z = 0
         self.pose = trans_pose
 #        common_time = self.tf.getLatestCommonTime('/ar_marker_1', '/map')
 #        self.pose.header.stamp = common_time
