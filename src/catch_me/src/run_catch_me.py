8d39314767125e6d1ef8da2391d9ce53310f7e6a
sputnik <team7@c1.pr2>
2013-12-02 01:33:07 -0800

    update rate while moving set to 3, otherwise 0.5

--- src/catch_me/src/run_catch_me.py
+++ src/catch_me/src/run_catch_me.py
@@ -6,10 +6,7 @@ roslib.load_manifest('simple_navigation_goals')
 import rospy
 import tf
 import actionlib
-from geometry_msgs.msg import Pose, PoseStamped, Point, Quaternion
-from visualization_msgs.msg import Marker
-from head_object_tracking import HeadObjectTracking
-from move_base_msgs.msg import MoveBaseAction, MoveBaseActionGoal, MoveBaseGoal
+from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
 from catch_me import srv
 import threading
 from catch_me.msg import *
@@ -17,8 +14,9 @@ from actionlib_msgs.msg import GoalStatus
 import copy 
 
 class CatchMe:
+  FOLLOW_DIST = .5
+
   def __init__(self):
-    self.MIN_GOAL_TIME_S = 5
     self._last_move = 0
     self._last_pose = None
 
@@ -39,57 +37,72 @@ class CatchMe:
     self.head_s_client.wait_for_server()
     rospy.loginfo('Head-search action client started')
 
-    threading.Timer(0, self.move_to_marker).start()
+    threading.Timer(0, self.follow_marker).start()
 
-  def move_to_marker(self):
+  def follow_marker(self):
     pose_stamped = self.destination_service()
     
-    if pose_stamped is None or pose_stamped.pose.header.stamp.secs == 0:
-      rospy.loginfo('No marker position found')
+    if (pose_stamped is None or
+          pose_stamped.pose.header.stamp.secs == 0 or
+          pose_stamped.pose == self._last_pose):
+      # Either, no marker known, or marker is old
+      if pose_stamped.pose == self._last_pose:
+        rospy.loginfo('Marker position unchanged')
+      else:
+        rospy.loginfo('No marker position found')
+
       if self.head_s_client.get_state() != GoalStatus.ACTIVE:
         goal = LocalSearchGoal(True)
         self.head_s_client.send_goal(goal)
+
     else:
-      rospy.loginfo('Moving towards marker')
+      # New marker seen, try to move to it
       pos = pose_stamped.pose.pose.position
       orient = pose_stamped.pose.pose.orientation
       pos_frame_id = pose_stamped.pose.header.frame_id
       rospy.loginfo(str(pos_frame_id) + '\n' + str(pos.x) + ',' + str(pos.y) + ',' + str(pos.z))
 
-      if pose_stamped.pose == self._last_pose:
-        rospy.loginfo('Marker position unchanged')
-        if self.head_s_client.get_state() != GoalStatus.ACTIVE:
-          goal = LocalSearchGoal(True)
-          self.head_s_client.send_goal(goal)
-
       if pose_stamped.pose != self._last_pose or self.base_client.get_state() != GoalStatus.SUCCEEDED:
+        rospy.loginfo('Marker position updated.  Moving towards marker.')
         self._last_pose = copy.deepcopy(pose_stamped.pose)
-        pose = pose_stamped.pose
-        pose.header.stamp = rospy.Time.now()
 
-        goal = MoveBaseGoal()
-        goal.target_pose = pose
+        self.move_to_marker(pose_stamped)
 
-        # The robot can't move in these directions. Clear them so it's not confused
-        goal.target_pose.pose.position.z = 0
-        goal.target_pose.pose.orientation.x = 0
-        goal.target_pose.pose.orientation.y = 0
-
-
-        # If the marker position is fresh, stay behind it so we don't collide.
-        # Use pose_stamped to get original time
-        if pose_stamped.pose.header.stamp.secs + 2 > rospy.Time.now().secs:
-          goal.target_pose.pose.position.x = goal.target_pose.pose.position.x - 0.8
+    goal_status = self.base_client.get_state()
+    if goal_status == GoalStatus.PENDING or goal_status == GoalStatus.ACTIVE:
+      # if we are currently moving/planning, slow update rate
+      update_rate = 3
+    else:
+      # if we are currently a sitting duck, fast update rate
+      update_rate = 0.5
+    threading.Timer(update_rate, self.follow_marker).start()
 
 
-        rospy.loginfo('Sending new goal. x: ' + str(goal.target_pose.pose.position.x) + ', y: ' + str(goal.target_pose.pose.position.y))
-        # Cancel old goal if it exists.
-        self.base_client.cancel_all_goals()
-        self.base_client.send_goal(goal)
-        #self.base_client.wait_for_result()
-        #rospy.loginfo(str(self.base_client.get_result()))
+  def move_to_marker(self, pose_stamped):
+    pose = pose_stamped.pose
+    pose.header.stamp = rospy.Time.now()
 
-    threading.Timer(self.MIN_GOAL_TIME_S, self.move_to_marker).start()
+    goal = MoveBaseGoal()
+    goal.target_pose = pose
+
+    # The robot can't move in these directions. Clear them so it's not confused
+    goal.target_pose.pose.position.z = 0
+    goal.target_pose.pose.orientation.x = 0
+    goal.target_pose.pose.orientation.y = 0
+
+
+    # If the marker position is fresh, stay behind it so we don't collide.
+    # Use pose_stamped to get original time
+    if pose_stamped.pose.header.stamp.secs + 2 > rospy.Time.now().secs:
+      goal.target_pose.pose.position.x = goal.target_pose.pose.position.x - self.FOLLOW_DIST
+
+
+    rospy.loginfo('Sending new goal. x: ' + str(goal.target_pose.pose.position.x) + ', y: ' + str(goal.target_pose.pose.position.y))
+    # Cancel old goal if it exists.
+    self.base_client.cancel_all_goals()
+    self.base_client.send_goal(goal)
+    #self.base_client.wait_for_result()
+    #rospy.loginfo(str(self.base_client.get_result()))
 
 if __name__=='__main__':
     rospy.init_node('catch_me_node')
